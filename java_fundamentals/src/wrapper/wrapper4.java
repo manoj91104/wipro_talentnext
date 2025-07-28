93d141ed6f3ac655fcc9227dedde1a5646453195
manoj91104 <ponnamanojmanoj@gmail.com>
2025-07-28 15:56:42 +0530

    jul 28 = oops_mini_project

--- java_fundamentals/src/wrapper/wrapper4.java
+++ java_fundamentals/src/wrapper/wrapper4.java
@@ -1,50 +1,43 @@
 package wrapper;
-import java.util.*;
+class Employee implements Cloneable{
+	int id;
+	String name;
+	Employee(int id,String name){
+		this.id = id;
+		this.name = name;
+	}
+	public Employee clone() {
+		try {
+			return (Employee) super.clone();
+		}
+		catch(CloneNotSupportedException e) {
+			return null;
+		}
+	}
+	public void display() {
+		System.out.println("Id : "+id+" ," +"Name : "+name);
+	}
+}
+
 public class wrapper4 {
-	class Employee implements Cloneable {
-	    int id;
-	    String name;
-	    double salary;
 
-	    public Employee(int id, String name, double salary) {
-	        this.id = id;
-	        this.name = name;
-	        this.salary = salary;
-	    }
-
-	    public Employee clone() {
-	        try {
-	            return (Employee) super.clone(); // Shallow copy
-	        } catch (CloneNotSupportedException e) {
-	            System.out.println("Cloning not supported.");
-	            return null;
-	        }
-	    }
-
-	    public void display() {
-	        System.out.println("ID: " + id + ", Name: " + name + ", Salary: " + salary);
-	    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee emp1 = new Employee(111,"Vinay");
+		Employee emp2 = emp1.clone();
+		System.out.println("Before Modification: ");
+		System.out.print("Original= ");
+		emp1.display();
+		System.out.print("Clone= ");
+		emp2.display();
+		
+		emp1.name="Sukumar";
+		emp1.id = 21;
+		System.out.print("\nAfter Modification: ");
+		System.out.print("\nOriginal= ");
+		emp1.display();
+		System.out.print("Clone= ");
+		emp2.display();
 	}
-
-	public class Main {
-	    public static void main(String[] args) {
-	        Employee emp1 = new Employee(101, "John", 50000.0);
-	        System.out.println("Original Employee:");
-	        emp1.display();
-
-	        Employee emp2 = emp1.clone();
-
-	        emp1.name = "David";
-	        emp1.salary = 60000.0;
-
-	        System.out.println("\nAfter modifying original employee:");
-	        System.out.println("Original Employee:");
-	        emp1.display();
-
-	        System.out.println("Cloned Employee:");
-	        emp2.display();
-	    }
-	}
-
 
 }
